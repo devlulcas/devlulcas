423fbd727f2f9212d05608a200f7d3fe2ed177aa
devlulcas <lucasalvesrego.contato@gmail.com>
2021-09-08 11:37:45 -0300

    changed: openMenu from arrow func to normal func

--- javascript/main.js
+++ javascript/main.js
@@ -5,10 +5,10 @@ const mobileMenu = document.querySelector("[data-open]");
 //Open and closes the mobile navigation menu
 buttonMenu.onclick = () => openMenu();
 optionsMenu.onclick = () => openMenu();
-const openMenu = () => {
+
+function openMenu() {
   const menuState = mobileMenu.dataset;
   buttonMenu.classList.toggle("menu-btn-animate");
   //Closes the menu if it's open, Opens the menu if it's closed
-  menuState.open = (menuState.open === "true") ? "false" : "true";
+  menuState.open = menuState.open === "true" ? "false" : "true";
 }
-
